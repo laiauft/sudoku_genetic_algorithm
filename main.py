a8445873454beb9d73e43726ee97d004eb4b035b
armanoalves <armanobarrosalvesh@gmail.com>
2023-05-11 17:33:50 -0300

    Ajustes nos arquivos e adição da função de seleção de pais

--- main.py
+++ main.py
@@ -1,58 +1,15 @@
-import sudoku_structure
-import random
+from algorithms.genetic_algorithm import calculate_fitness, generate_population, roulette_selection
+import problem.sudoku_structure as sudoku_structure
+import sys
 
-pop_size = 2000
-
-def generate_population(puzzle, pop_size):
-  population = []
-  for _ in range(pop_size):
-    individual = []
-    for row in puzzle:
-      new_row = []
-      for val in row:
-        if val == 0:
-          new_row.append(random.randint(1, 9))
-        else:
-          new_row.append(val)
-      individual.append(new_row)
-    population.append(individual)
-  return population
-
-def rate_fitness_rollet(population, pop_size):
-  # Every individual must have a fitness
-  # So, an array will storage the value of fitness for every individual
-  # The fitness_list[0] is for the first individual: population[0]
-  
-  fitness_list = [0] * pop_size
-  total_errors_list = [0] * pop_size
-  num = 216
-  
-  # Loop that iterates the value of pop_size
-  for i in range(pop_size):
-    
-    for j in range(len(population[i])):
-      rowsWithoutDuplicates = [*set(population[i][j])]
-      errorsInRow = len(population[i]) - len(rowsWithoutDuplicates)
-      total_errors_list[i] += errorsInRow
-
-      population_columns = sudoku_structure.categorize_columns(population[i])
-      columnsWithoutDuplicates = [*set(population_columns[j])]
-      errorsInColumn = len(population[i]) - len(columnsWithoutDuplicates)
-      total_errors_list[i] += errorsInColumn
-      
-      population_quadrants = sudoku_structure.categorize_quadrants(population[i])
-      quadrantsWithoutDuplicates = [*set(population_quadrants[j])]
-      errorsInQuadrant = len(population[i]) - len(quadrantsWithoutDuplicates)
-      total_errors_list[i] += errorsInQuadrant
-      
-    fitness_list[i] = num - total_errors_list[i]
-      
-  for i in range(len(fitness_list)):
-    print(f"Fitness of individual {i} --> {fitness_list[i]}")   
+args = sys.argv
+pop_size = 100
 
 def main():
-  print("Insert the puzzle as string: ")
-  puzzle_string = input()
+  print("Starting...")
+  with open(args[1], 'r') as arq:
+    puzzle_string = arq.read()
+
   puzzle = sudoku_structure.string_to_array(puzzle_string)
   
   menu_state = True
@@ -90,14 +47,19 @@ def main():
     elif option == 5:
       population = generate_population(puzzle, pop_size)
       print("")
-      for i in range(pop_size):
-        print(f"Individual {i}: {population[i]}")
+      # for i in range(pop_size):
+      #   print(f"Individual {i}: {population[i]}")
     
     elif option == 6:
       if len(population) == 0:
         print("You must generate initial population first.")
       else:
-        rate_fitness_rollet(population, pop_size)
+        fitness = calculate_fitness(population)
+        parent1 = roulette_selection(population, fitness)
+        parent2 = roulette_selection(population, fitness)
+
+        print("Parent 1:", parent1)
+        print("Parent 2:", parent2)
     
     elif option == 0:
       print("Exiting.")
