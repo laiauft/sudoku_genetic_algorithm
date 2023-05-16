95627e0ed99ccd2e814e188d4a3303c988431050
erarich <ericksantos@mailfence.com>
2023-05-16 14:47:39 -0300

    Refactored again

--- src/genetic/crossing_pairs.py
+++ src/genetic/crossing_pairs.py
@@ -1,30 +1,12 @@
 import math
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 from genetic.population import Population
 
 def crossing_individuals(parent1, parent2, population: Population):
-=======
-def crossing_individuals(population, pop_size):
->>>>>>> parent of 322ce0b (FEAT new objects created in genetic module)
-=======
-def crossing_individuals(population, pop_size):
->>>>>>> parent of 322ce0b (FEAT new objects created in genetic module)
 	children_population = [] 
 
-	children_count = int(pop_size/2)
+	children_count = int(population.size/2)
 	for i in range(children_count):
-<<<<<<< HEAD
-=======
-		### OBSERVAÇÃO
-		#   por enquanto o pai2 de uma criança será 
-		# o pai1 da proxima por conta do contador
-		parents = [population[i], population[i+1]]
-<<<<<<< HEAD
->>>>>>> parent of 322ce0b (FEAT new objects created in genetic module)
-=======
->>>>>>> parent of 322ce0b (FEAT new objects created in genetic module)
 
 		cutoff = math.ceil(len(parent1)/2)
 		genes = parent1[:cutoff] + parent2[cutoff:]
@@ -32,23 +14,7 @@ def crossing_individuals(population, pop_size):
 		children_population.append(genes)
 		i = i + 1
 	
-<<<<<<< HEAD
-<<<<<<< HEAD
 	m = children_count
 	new_population = population.individuals[:m] + children_population
 
 	population.individuals = new_population
-=======
-=======
->>>>>>> parent of 322ce0b (FEAT new objects created in genetic module)
-	m = children_count # m = numero de indivíduos antigos mantidos
-	new_population = population[:m] + children_population
-	#   A nova população é gerada pelo m indivíduos mantidos 
-	# concatenada com a lista de crianças geradas com o tamanho
-	# definido como `children_count = int(pop_size/2)`. 
-<<<<<<< HEAD
-	return new_population
->>>>>>> parent of 322ce0b (FEAT new objects created in genetic module)
-=======
-	return new_population
->>>>>>> parent of 322ce0b (FEAT new objects created in genetic module)
--- src/main.py
+++ src/main.py
@@ -7,14 +7,8 @@ from genetic.crossing_pairs import crossing_individuals
 
 args = sys.argv
 pop_size = 100
-<<<<<<< HEAD
-<<<<<<< HEAD
 max_fitness = 216
 max_generations = 1000
-=======
->>>>>>> parent of 322ce0b (FEAT new objects created in genetic module)
-=======
->>>>>>> parent of 322ce0b (FEAT new objects created in genetic module)
 
 def main():
     with open(args[1], 'r') as arq:
@@ -26,8 +20,6 @@ def main():
     print("Starting genetic algorithm...")
     print(".\n.\n.")
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 
     ## Step 1: Generate initial random population
     population = Population(puzzle, pop_size)
@@ -35,16 +27,6 @@ def main():
 
     ## Step 2: Calculate fitness of individuals
     fitness = population.calculate()
-=======
-    population = generate_population(puzzle, pop_size)
-    print("Generated initial population...")
-    fitness = calculate_fitness(population)
->>>>>>> parent of 322ce0b (FEAT new objects created in genetic module)
-=======
-    population = generate_population(puzzle, pop_size)
-    print("Generated initial population...")
-    fitness = calculate_fitness(population)
->>>>>>> parent of 322ce0b (FEAT new objects created in genetic module)
     print("Initial population's fitness calculated...")
 
     fitness_average = np.mean(fitness)
@@ -59,25 +41,14 @@ def main():
     criteria = True
     generation = 0
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     while criteria and generation < max_generations:
         print("Generation:", generation)
-=======
-    parent1 = roulette_selection(population, fitness)
-    parent2 = roulette_selection(population, fitness)
->>>>>>> parent of 322ce0b (FEAT new objects created in genetic module)
-=======
-    parent1 = roulette_selection(population, fitness)
-    parent2 = roulette_selection(population, fitness)
->>>>>>> parent of 322ce0b (FEAT new objects created in genetic module)
 
         parent1 = roulette_selection(population)
         parent2 = roulette_selection(population)
 
         crossing_individuals(parent1, parent2, population)
 
-<<<<<<< HEAD
         population.individuals = mutate_individuals(population, puzzle)
 
         if max(fitness) == max_fitness:
@@ -91,42 +62,6 @@ def main():
         print("Maximum number of generations reached.")
     else:
         print("Best individual:", best_individual)
-=======
-    while(menu_state):
-        print(20*"--")
-        print("Choose the options below: ")
-        print("1. Display population individuals.")
-        print("2. Generate children population.")
-        print("3. Mutate individuals in population.")
-        print("0. Quit the program.")
-        print(20*"--")
-      
-        option = int(input("--> Option: "))
-        print("\n\n")
-
-        if option == 1:
-            if len(population) == 0:
-                print("You must generate initial population first.")
-            else: 
-                for i in range(pop_size):
-                    print(f"Individual {i}: {population[i]}")
-
-        elif option == 2:
-            if len(population) == 0: 
-                print("You must generate initial population first.")
-            else: 
-                population = crossing_individuals(population, pop_size)
-
-        elif option == 3:
-            if len(population) == 0: 
-                print("You must generate initial population first.")
-            else: 
-                population = mutate_individuals(population, puzzle)
-
-        elif option == 0:
-            print("Exiting.")
-            break
->>>>>>> parent of 322ce0b (FEAT new objects created in genetic module)
     
 if __name__ == '__main__':
     main()
